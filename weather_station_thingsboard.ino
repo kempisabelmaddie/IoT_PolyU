fe39d66bbe4abe2876c51efc31703d318d49ba5e
isabelmaddiek <56757186+isabelmaddiek@users.noreply.github.com>
2021-01-25 11:08:32 +0800

    Update weather_station_thingsboard.ino

--- weather_station_thingsboard.ino
+++ weather_station_thingsboard.ino
@@ -117,5 +117,5 @@ void reconnect() {
   }
 }
 
-//Referece:
+//Reference:
 //ThingsBoard (2021) Temperature upload over MQTT using ESP8266 and DHT22 sensor (2021) [Source Code]. https://thingsboard.io/docs/samples/esp8266/temperature/.
