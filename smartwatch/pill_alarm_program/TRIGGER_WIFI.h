7daec8b75bf63ac4958994052b4c95bc2c4b4db0
kempisabelmaddie <80112384+kempisabelmaddie@users.noreply.github.com>
2021-06-18 18:08:16 +0800

    Add files via upload

---
+++ smartwatch/pill_alarm_program/TRIGGER_WIFI.h
@@ -0,0 +1,35 @@
+#ifndef __TRIGGER_WIFI_H__
+#define __TRIGGER_WIFI_H__
+
+#include <WiFiClientSecure.h>
+
+WiFiClientSecure client;
+
+#define Debug_Serial_Mon
+
+
+void WIFI_Connect(const char* wifi_id, const char *wifi_password)
+{
+  #ifdef Debug_Serial_Mon
+	  Serial.print("connecting to ");
+	  Serial.println(wifi_id);
+  #endif
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_id, wifi_password);
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  #ifdef Debug_Serial_Mon
+	  Serial.println("");
+	  Serial.println("WiFi connected");
+	  Serial.println("IP address: ");
+	  Serial.println(WiFi.localIP());
+  #endif
+}
+
+#endif
